32e877cf560e9a29b02fdfbb3a8394582b880849
ARUN NISHAD <93611973+ARnishad45@users.noreply.github.com>
2024-01-05 23:25:17 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (58.61%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if (s.length() != goal.length()) {
+            return false;
+        }
+
+        string temp = goal + goal;
+
+        return temp.find(s) != string::npos;
+    }
+};
