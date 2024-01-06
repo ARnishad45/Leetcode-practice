1a5cb2cbffe6b9022b854b507500075dc937176f
ARUN NISHAD <93611973+ARnishad45@users.noreply.github.com>
2024-01-06 23:57:26 +0530

    Time: 0 ms (100.00%), Space: 12.5 MB (55.00%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
+        int i = nums.size() - 2;
+
+        // Find the first element that breaks descending order
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
+        if (i >= 0) {
+            int j = nums.size() - 1;
+
+            // Find the smallest element to the right of i that is larger than i
+            while (j >= 0 && nums[j] <= nums[i]) {
+                j--;
+            }
+
+            swap(nums[i], nums[j]);
+        }
+        reverse(nums.begin() + i + 1, nums.end());
     }
 };
