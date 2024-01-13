3e226731f4ed3dfadc265529223a6229f01946db
ARUN NISHAD <93611973+ARnishad45@users.noreply.github.com>
2024-01-13 18:57:22 +0530

    Time: 0 ms (100.00%), Space: 8.8 MB (74.15%) - LeetHub

---
+++ 0274-h-index/0274-h-index.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int low = 0 , high = citations.size();
+        while(low < high){
+            int mid = (low+high+1)/2;
+            int cnt = 0;
+            for(int i=0 ; i<citations.size(); i++)
+            if(citations[i] >= mid) 
+                cnt++;
+            if(cnt >= mid) 
+                low = mid;
+            else 
+                high = mid-1;
+        }
+        return low;
+    }
+};
